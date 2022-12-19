f500ed687410fecdb3e9d1e47ff84e02fd4a2582
Matthieu Houdebine <mat.houdebine@wanadoo.fr>
2022-12-19 14:43:39 +0100

    Add a cross-platform tray icon with Exit menu entry

--- main.py
+++ main.py
@@ -3,9 +3,12 @@
 # https://github.com/mathoudebine/turing-smart-screen-python
 import locale
 import os
+import pystray
 import signal
 import sys
 import time
+
+from PIL import Image
 
 MIN_PYTHON = (3, 7)
 if sys.version_info < MIN_PYTHON:
@@ -24,9 +27,8 @@ if __name__ == "__main__":
     # Apply system locale to this program
     locale.setlocale(locale.LC_ALL, '')
 
-    def sighandler(signum, frame):
-        logger.info(" Caught signal %d, exiting" % signum)
 
+    def clean_stop(tray_icon=None):
         # Do not stop the program now in case data transmission was in progress
         # Instead, ask the scheduler to empty the action queue before stopping
         scheduler.STOPPING = True
@@ -41,11 +43,25 @@ if __name__ == "__main__":
 
         logger.debug("(%.1fs)" % (5 - wait_time))
 
+        # Remove icon just before the exit
+        if tray_icon:
+            tray_icon.visible = False
+
         # We force the exit to avoid waiting for other scheduled tasks: they may have a long delay!
         try:
             sys.exit(0)
         except:
             os._exit(0)
+
+
+    def sighandler(signum, frame=None):
+        logger.info("Caught signal %d, exiting" % signum)
+        clean_stop()
+
+
+    def on_exit_tray(tray_icon, item):
+        logger.info("Exit from tray icon")
+        clean_stop(tray_icon)
 
 
     # Set the signal handlers, to send a complete frame to the LCD before exit
@@ -81,3 +97,14 @@ if __name__ == "__main__":
     scheduler.NetStats()
     scheduler.DateStats()
     scheduler.QueueHandler()
+
+    # Create a tray icon for the program, with an Exit entry in menu
+    tray_icon = pystray.Icon(
+        name='Turing System Monitor',
+        title='Turing System Monitor',
+        icon=Image.open("res/icons/724873501557740364-64.png"),
+        menu=pystray.Menu(
+            pystray.MenuItem(
+                'Exit',
+                on_exit_tray))
+    ).run()
