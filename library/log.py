9214d44a633ca3d43860e870fd7bd7a1e83b0dab
Matthieu Houdebine <mat.houdebine@wanadoo.fr>
2023-02-22 13:46:17 +0100

    Log to rolling file max 1MB, with date/time formatted from locale

--- library/log.py
+++ library/log.py
@@ -17,17 +17,20 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # Configure logging format
+import locale
 import logging
-from datetime import datetime
-date = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+from logging.handlers import RotatingFileHandler
 
-logging.basicConfig(# format='%(asctime)s [%(levelname)s] %(message)s in %(pathname)s:%(lineno)d',
-                    format="%(asctime)s [%(levelname)s] %(message)s",
-                    handlers=[
-                        # logging.FileHandler("log_"+date+".log", mode='w'),  # Log in textfile
-                        logging.StreamHandler()  # Log also in console
-                    ],
-                    datefmt='%H:%M:%S')
+# use current locale for date/time formatting in logs
+locale.setlocale(locale.LC_ALL, '')
+
+logging.basicConfig(  # format='%(asctime)s [%(levelname)s] %(message)s in %(pathname)s:%(lineno)d',
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        RotatingFileHandler("log.log", maxBytes=1000000, backupCount=0),  # Log in textfile max 1MB
+        logging.StreamHandler()  # Log also in console
+    ],
+    datefmt='%x %X')
 
 logger = logging.getLogger('turing')
 logger.setLevel(logging.DEBUG)  # Lowest log level : print all messages
