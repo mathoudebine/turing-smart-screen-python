2e8fb1f63742a5aec150280c0c747dcbae415d5a
smandon <sylvainmandon@yahoo.fr>
2023-05-14 13:51:14 +0200

    Update stats.py

--- library/stats.py
+++ library/stats.py
@@ -26,6 +26,9 @@ import math
 import os
 import platform
 import sys
+import time
+
+from statistics import mean
 
 import babel.dates
 from psutil._common import bytes2human
@@ -374,7 +377,11 @@ def display_gpu_stats(load, memory_percentage, memory_used_mb, temperature):
 class Gpu:
     @staticmethod
     def stats():
-        load, memory_percentage, memory_used_mb, temperature = sensors.Gpu.stats()
+        load_values = []
+        for i in range(100):
+            load, memory_percentage, memory_used_mb, temperature = sensors.Gpu.stats()
+            load_values.append(load)
+        load = mean(load_values)
         display_gpu_stats(load, memory_percentage, memory_used_mb, temperature)
 
     @staticmethod
